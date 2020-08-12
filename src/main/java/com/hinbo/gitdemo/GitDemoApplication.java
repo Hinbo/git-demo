77b3ebfd85a94ed660fe703a74797a770977d8cc
hinbo <981640886@qq.com>
2020-08-12 23:17:04 +0800

    [变更]类名修改

--- src/main/java/com/hinbo/gitdemo/GitDemoApplication.java
+++ src/main/java/com/hinbo/gitdemo/GitDemoApplication.java
@@ -3,11 +3,14 @@ package com.hinbo.gitdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Hinbo
+ */
 @SpringBootApplication
-public class GitdemoApplication {
+public class GitDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GitdemoApplication.class, args);
+        SpringApplication.run(GitDemoApplication.class, args);
     }
 
 }
